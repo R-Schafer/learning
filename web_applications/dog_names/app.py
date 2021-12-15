c5e45ff91baa8cee589a4bf2c4784dcb57a03f98
Rainey Schafer <schafer.rainey@gmail.com>
2021-12-15 16:32:20 -0600

    Added list for dog names, added request method

--- web_applications/dog_names/app.py
+++ web_applications/dog_names/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+DOG_NAMES = []
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == "POST":
+        DOG_NAMES.append(request.form['dog_name'])
+
+    return render_template('index.html', dogs=DOG_NAMES)
