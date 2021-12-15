490324a3df40a13ef24acae7dccd5551f3eba83b
Rainey Schafer <schafer.rainey@gmail.com>
2021-12-15 11:54:48 -0600

    Updated template to render dynamic data

--- web_applications/todo/app.py
+++ web_applications/todo/app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return render_template('todo.html')
+def index():
+    return render_template('todo.html', todos=[
+        {"action": "feed dog", "done": True, "due": False},
+        {"action": "eat breakfast", "done": False, "due": False},
+        {"action": "eat lunch", "done": False, "due": True},
+    ])
