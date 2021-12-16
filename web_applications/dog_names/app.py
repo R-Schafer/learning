b9f1d9e3370dcd73bae9c590f7a808789bd8668b
Rainey Schafer <schafer.rainey@gmail.com>
2021-12-16 13:08:22 -0600

    Added 'remove' button to remove unwanted dog names

--- web_applications/dog_names/app.py
+++ web_applications/dog_names/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 DOG_NAMES = []
@@ -6,8 +6,13 @@ DOG_NAMES = []
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        dog_name = request.form['dog_name']
-        if dog_name.strip() != "":
-            DOG_NAMES.append(dog_name)
+        if "add_name" in request.form:
+            dog_name = request.form['add_name']
+            if dog_name.strip() != "":
+                DOG_NAMES.append(dog_name)
+
+        elif "delete_name" in request.form:
+            dog_name = request.form['delete_name']
+            DOG_NAMES.remove(dog_name)
 
     return render_template('index.html', dogs=DOG_NAMES)
