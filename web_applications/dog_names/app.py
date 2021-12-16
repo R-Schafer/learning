10d5c78088ac3c6df2e40e15b9a7777c7cd43fa3
Rainey Schafer <schafer.rainey@gmail.com>
2021-12-16 16:19:08 -0600

    Added up-vote and down-vote buttons

--- web_applications/dog_names/app.py
+++ web_applications/dog_names/app.py
@@ -15,4 +15,16 @@ def index():
             dog_name = request.form['delete_name']
             DOG_NAMES.remove(dog_name)
 
+        elif "move_up" in request.form:
+            dog_name = request.form['move_up']
+            position = DOG_NAMES.index(dog_name)
+            if position > 0:
+                DOG_NAMES[position - 1], DOG_NAMES[position] = dog_name, DOG_NAMES[position - 1]
+
+        elif "move_down" in request.form:
+            dog_name = request.form['move_down']
+            position = DOG_NAMES.index(dog_name)
+            if position < len(DOG_NAMES) - 1:
+                DOG_NAMES[position + 1], DOG_NAMES[position] = dog_name, DOG_NAMES[position + 1]
+
     return render_template('index.html', dogs=DOG_NAMES)
